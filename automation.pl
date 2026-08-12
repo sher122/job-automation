4c7f6b6fc77d36f9c7006672b15f57df69497d50
Sher122 <sinhashrestha2222@gmail.com>
2026-08-12 21:36:37 +0530

    load jobs from JSON input

--- automation.pl
+++ automation.pl
@@ -2,7 +2,24 @@
 
 use strict;
 use warnings;
+use JSON::PP;
 
 my $project_name = "Job Automation Tool";
+my $job_file     = "jobs/jobs.json";
 
 print "Starting $project_name\n";
+
+open(my $file_handle, "<", $job_file)
+    or die "Cannot open $job_file: $!\n";
+
+local $/;
+
+my $json_text = <$file_handle>;
+
+close($file_handle);
+
+my $jobs = decode_json($json_text);
+
+my $job_count = scalar(@{$jobs});
+
+print "Loaded $job_count jobs\n";
